581662caf08e702e15049750fb27114604208e8a
Anim Xhafa <animxhafa@gmail.com>
2021-06-27 21:15:40 -0400

    added css

---
+++ evolution.js
@@ -0,0 +1,22 @@
+
+
+function createCanvas() {
+    var canvas = document.getElementById('canvas')
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d')
+    }
+}
+
+
+//Creation
+
+
+//Evaluation
+
+//Selection and Crossover
+
+//Reproduction
+
+//Mutation
+
+//Termination
