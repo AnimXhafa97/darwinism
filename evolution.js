e14cb30a6dd60268484ba305362949dd514e4318
AnimXhafa97 <animxhafa@gmail.com>
2021-08-04 18:17:01 -0400

    added bubble class

--- evolution.js
+++ evolution.js
@@ -1,4 +1,4 @@
-
+running = true;
 
 function createCanvas() {
     var canvas = document.getElementById('canvas')
@@ -7,19 +7,44 @@ function createCanvas() {
     }
 }
 
-//creating the dot class
+//helper function for getting a random integer from min - max, inclusive
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random()*(max-min+1)+min)
+}
 
 
+class Bubble {
 
-//Creation
+  constructor () {
+    this.x = 100
+    this.y = 200
+  }
 
+  move() {
+    this.x = this.x + getRandomInt(-5, 5)
+    this.y = this.y + getRandomInt(-5, 5)
+  }
 
-//Evaluation
+  show() {
+    var canvas = getElementById('canvas')
+    var ctx = canvas.getContext('2d')
 
-//Selection and Crossover
+    ctx.beginPath()
+    ctx.arc(100, 100, 25, 0, 2 * Math.PI)
+    ctx.fill();
+    ctx.stroke()
+  }
 
-//Reproduction
+}
 
-//Mutation
+function runAlgo() {
+  while (running) {
+    let bubble = Bubble()
+  }
+}
 
-//Termination
+function stopAlgo() {
+  running = false;
+}
